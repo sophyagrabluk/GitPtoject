1631345f0cc12bd687703b5df98c93dd931fbc9d
Sofia <sgrabluk5@gmail.com>
2022-09-28 21:24:17 +0300

    tdytty

--- src/SecondClassGit.java
+++ src/SecondClassGit.java
@@ -2,5 +2,7 @@ public class SecondClassGit {
     public static void main(String[] args) {
         System.out.println("Hello GitWorld!");
         System.out.println("Hello GitWorld!");
+
+        System.out.println("разработка");
     }
 }
