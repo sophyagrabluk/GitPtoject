54b14ac9d307de6f3f487911b647ab140543d06a
Sofia <sgrabluk5@gmail.com>
2022-10-08 19:09:46 +0300

    HomeWork Arrays

--- src/HW_Arrays.java
+++ src/HW_Arrays.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class HW_4 {
+public class HW_Arrays {
     public static void main(String[] args) {
           Scanner scanner = new Scanner(System.in);
 
