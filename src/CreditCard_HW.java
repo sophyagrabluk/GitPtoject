317514c07df33f80f91d54e82fe730fd7eb0391f
Sofia <sgrabluk5@gmail.com>
2022-10-12 14:58:57 +0300

    HomeWork 6 (Class and Methods)

--- src/CreditCard_HW.java
+++ src/CreditCard_HW.java
@@ -1,24 +1,30 @@
 public class CreditCard_HW {
+    // объявили поля (переменные)
     long account_number;
     int current_balance;
 
-    public CreditCard_HW(long accnum, int currbal) {
-        this.account_number = accnum;
-        this.current_balance = currbal;
+    // создали конструктор (пользовательский)
+    public CreditCard_HW(long account_number, int current_balance) {
+        this.account_number = account_number;
+        this.current_balance = current_balance;
     }
 
-// Добавьте метод, который позволяет начислять сумму на кредитную карточку.
+    // Добавьте метод, который позволяет начислять сумму на кредитную карточку.
     int addMoney(int current_balance, int x) {
+
         return current_balance + x;
     }
-// Добавьте метод, который позволяет снимать с карточки некоторую сумму.
+
+    // Добавьте метод, который позволяет снимать с карточки некоторую сумму.
     int takeMoney(int current_balance, int x) {
+
         return current_balance - x;
     }
-// Добавьте метод, который выводит текущую информацию о карточке.
+
+    // Добавьте метод, который выводит текущую информацию о карточке.
+
     void showAllInfo() {
         System.out.println("Номер карты: " + account_number + ", Текущий счёт: " + current_balance);
     }
-
 
 }
--- src/HW_6_ClassMethods.java
+++ src/HW_6_ClassMethods.java
@@ -1,23 +1,21 @@
 public class HW_6_ClassMethods {
     public static void main(String[] args) {
-     /*   ПРИМЕР
-     CreditCard_HW creditCard1 = new CreditCard_HW(123456789, 1200);
 
-        System.out.println(creditCard1.addMoney(1200, 500));
-        System.out.println(creditCard1.takeMoney(1200, 100));
-        creditCard1.showAllInfo();
-     */
-
+     // Напишите программу,которая создает три объекта класса CreditCard,
+     // у которых заданы номер счета и начальная сумма
         CreditCard_HW creditcardAlfa = new CreditCard_HW(123456789, 2600); // создали 1-ый объект
         CreditCard_HW creditcardBelInvest = new CreditCard_HW(1133557799, 1200); // создали 2-ой объект
         CreditCard_HW creditcardSber = new CreditCard_HW(22446688, 650); // создали 3-ий объект
 
-        System.out.println(creditcardAlfa.addMoney(creditcardBelInvest.current_balance, 500));
-        System.out.println(creditcardBelInvest.addMoney(creditcardBelInvest.current_balance, 50));
-        System.out.println(creditcardSber.takeMoney(creditcardSber.current_balance, 240));
+        // Положите деньги на первые две карточки и снимите с третьей
+        System.out.println(creditcardAlfa.addMoney(creditcardBelInvest.current_balance, 500)); // положили деньги на 1-ую карту
+        System.out.println(creditcardBelInvest.addMoney(creditcardBelInvest.current_balance, 50)); // положили деньги на 2-ую карту
+        System.out.println(creditcardSber.takeMoney(creditcardSber.current_balance, 240)); // сняли деньги с 3-ей карты
 
+        // Выведите на экран текущее состояние всех трех карточек
         creditcardAlfa.showAllInfo();
         creditcardBelInvest.showAllInfo();
         creditcardSber.showAllInfo();
+
     }
 }
