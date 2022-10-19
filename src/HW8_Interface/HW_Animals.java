b1d2c85bab954a5570162c67d58bd85c4664cf13
Sofia <sgrabluk5@gmail.com>
2022-10-19 13:44:16 +0300

    HW8, Interfaces (Cow, Tiger)2

---
+++ src/HW8_Interface/HW_Animals.java
@@ -0,0 +1,9 @@
+package HW8_Interface;
+public class HW_Animals {
+        public static void main(String[] args) {
+            Tiger tiger = new Tiger();
+            Cow cow = new Cow();
+            tiger.eat();
+            cow.eat();
+        }
+}
