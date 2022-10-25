afa148850acd236279886a2dfe1d1c875a00828e
Sofia <sgrabluk5@gmail.com>
2022-10-25 19:06:17 +0300

    Сортировка пузырьком

---
+++ src/HW10_String/SortOfStrings.java
@@ -0,0 +1,31 @@
+package HW10_String;
+
+import java.util.Scanner;
+
+public class SortOfStrings {
+    public static void main(String[] args) {
+        // Ввести 3 строки с консоли. Упорядочить и вывести строки в порядке возрастания значений их длины.
+        Scanner scanner = new Scanner(System.in);
+        String[] str = new String[3];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = scanner.nextLine();
+        }
+        System.out.println();
+
+
+        for (int i = str.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (str[j].length() > str[j + 1].length()) {
+                    String tmp = str[j];
+                    str[j] = str[j + 1];
+                    str[j + 1] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+
+    }
+}
