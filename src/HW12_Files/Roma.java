5e7fa37aac0a58c0dcfb1a20cf6e9fd2fb7ac832
Sofia <sgrabluk5@gmail.com>
2022-11-06 18:33:52 +0300

    HW12 (Files), Ромео и Джульетта

--- src/HW12_Files/Roma.java
+++ src/HW12_Files/Roma.java
@@ -1,13 +1,10 @@
 package HW12_Files;
-
-import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Roma {
     public static void main(String[] args) throws IOException {
-        //В задании приложен файл Ромео и Джульетта на английском, вычитать его в Idea.
-        //Проанализировать и записать в другой файл самое длинное слово.
         FileReader fileReader = new FileReader("C://TMS/romeo-and-juliet.txt");
         int i = -1;
         StringBuilder romeo = new StringBuilder();
@@ -16,9 +13,8 @@ public class Roma {
         }
         fileReader.close();
 
-        String romLJul = romeo.toString(); // преобразовали стрингБилдер в строку
-
-        String [] word = romLJul.split(" ");
+        String romeoJuletta = romeo.toString().replaceAll("\\W", " ");// \\W - значит не буквенно-цифровые символы
+        String[] word = romeoJuletta.split(" ");
         String longestWord = " ";
         for (int x = 0; x < word.length; x++) {
             if (word[x].length() >= longestWord.length()) {
@@ -26,5 +22,9 @@ public class Roma {
             }
         }
         System.out.println(longestWord);
+
+        FileWriter fileWriter = new FileWriter("C://TMS/longest_word_in_RJ.txt");
+        fileWriter.write(longestWord);
+        fileWriter.close();
     }
 }
