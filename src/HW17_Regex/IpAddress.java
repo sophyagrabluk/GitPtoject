2bc3958a9fb5aa3d1dd61d98eaf0a3f033b3255b
Sofia <sgrabluk5@gmail.com>
2022-11-22 15:26:28 +0300

    HW17 (Regex). Валидность IP адреса

---
+++ src/HW17_Regex/IpAddress.java
@@ -0,0 +1,24 @@
+package HW17_Regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IpAddress {
+    public static void main(String[] args) {
+        //IP-адрес – это строка чисел, разделенных точками. IP-адреса представляют собой набор из четырех чисел,
+        // например, 192.158.1.38.
+
+        // Написать программу для проверки на валидность введенного ip адреса. Пусть ip адрес задается с консоли.
+        // Программа должна проверять валидность введенного ip адреса с помощью регулярного выражения
+        // и выводить результат проверки на экран.
+        Scanner scanner = new Scanner(System.in);
+        String ipAddress = scanner.nextLine();
+        Pattern patternIP = Pattern.compile("^[0-9]{1,4}[.][0-9]{1,4}[.][0-9]{1,4}[.][0-9]{1,4}$");
+        Matcher matcherIP = patternIP.matcher(ipAddress);
+        if (matcherIP.find()) {
+            System.out.println("Your IP address is " + matcherIP.group());
+        } else
+            System.out.println("IP address is wrong");
+    }
+}
